f42e386e204d69209c85fc43c9ddeaa217935526
Firegame224 <xg474627@gmail.com>
2025-10-26 00:08:38 +0700

    docs(item): add ItemIncludeUserDto for enhanced item response with user details

--- src/modules/item/item.response.dto.ts
+++ src/modules/item/item.response.dto.ts
@@ -1,6 +1,9 @@
 import { HttpStatus } from "@nestjs/common";
 import { ApiProperty } from "@nestjs/swagger";
-import { $Enums, Item } from "@prisma/client";
+import { $Enums, Item, Prisma } from "@prisma/client";
+import { UserDto } from "../user/user.dto";
+
+type ItemIncludeUser = Prisma.ItemGetPayload<{ include: { user: true } }>
 
 export class ItemDto implements Item {
     @ApiProperty({ example: "xxxxxxxxxxxxxxxx" })
@@ -19,6 +22,25 @@ export class ItemDto implements Item {
     userId: string;
 }
 
+export class ItemIncludeUserDto implements ItemIncludeUser {
+    @ApiProperty({ example: "xxxxxxxxxxxxxxxx" })
+    itemId: string;
+    @ApiProperty({ example: "https://image-link.com" })
+    image: string;
+    @ApiProperty({ example: "Laptop Lenovo LOQ" })
+    itemName: string;
+    @ApiProperty({ example: "This is my laptop" })
+    itemInfo: string | null;
+    @ApiProperty({ example: "https://image-link.com" })
+    qrUrl: string | null;
+    @ApiProperty({ example: "TERSEDIA" })
+    status: $Enums.ItemStatus;
+    @ApiProperty({ example: "xxxxxxxxxxxxxxxx" })
+    userId: string;
+    @ApiProperty({ type: UserDto })
+    user: UserDto
+}
+
 export class CreateItemResponseDto {
     @ApiProperty({ example: "Item created Successfull" })
     message: string
@@ -35,24 +57,24 @@ export class GetItemByIdResponseDto {
     @ApiProperty({ example: HttpStatus.OK })
     statusCode: number
 
-    @ApiProperty({ type: ItemDto })
-    data: ItemDto
+    @ApiProperty({ type: ItemIncludeUserDto })
+    data: ItemIncludeUserDto
 }
 
 export class GetAllItemsResponseDto {
     @ApiProperty({ example: "Items retrieved Successfull" })
     message: string
-    @ApiProperty({ example: HttpStatus.CREATED })
+    @ApiProperty({ example: HttpStatus.OK })
     statusCode: number
 
-    @ApiProperty({ type: ItemDto, isArray: true })
-    data: ItemDto
+    @ApiProperty({ type: ItemIncludeUserDto, isArray: true })
+    data: ItemIncludeUserDto
 }
 
 export class GetuserItemsResponseDto {
     @ApiProperty({ example: "Items retrieved Successfull" })
     message: string
-    @ApiProperty({ example: HttpStatus.CREATED })
+    @ApiProperty({ example: HttpStatus.OK })
     statusCode: number
 
     @ApiProperty({ type: ItemDto, isArray: true })
