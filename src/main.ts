4a2866afc5fea28a107f5bfddd50c567ebc32cb9
Firegame224 <xg474627@gmail.com>
2025-10-25 20:45:40 +0700

    docs(): setup swagger for api documentations

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,14 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true
   }))
+  const config = new DocumentBuilder()
+    .setTitle("LoFo-API")
+    .setDescription("Find your lost items")
+    .setVersion("1.0")
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("/docs", app, documentFactory)
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`App listen on port ${process.env.PORT ?? 3000}`)
 }
 bootstrap();
