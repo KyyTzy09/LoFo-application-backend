bfa19f33326ffbfa50ec0d1eee156bfa15b0ea6a
Firegame224 <xg474627@gmail.com>
2025-10-26 00:28:29 +0700

    refactor: update base endpoint get response

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      name: "LoFo-Api",
+      status: "Active",
+      docs: "/docs",
+      createdBy: "Kyynotsepuh",
+      github: "https://github.com/KyyTzy09",
+      timeStamp: new Date().toLocaleDateString("id-ID", {
+        dateStyle: "full"
+      })
+    };
   }
 }
